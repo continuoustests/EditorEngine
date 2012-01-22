64d97831f1dfde0f64906d95b83b120aeb975363
acken <acken@acken.no>
2012-01-22 03:23:25 +0100

    Added refactoring support to filewriter

---
+++ src/EditorEngine.Core.Tests/Editors/FileWriterTests.cs
@@ -0,0 +1,129 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using EditorEngine.Core.Editors;
+using EditorEngine.Core.Arguments;
+using EditorEngine.Core.Messaging.Messages;
+
+namespace EditorEngine.Core.Tests.Editors
+{
+	[TestFixture]
+	public class FileWriterTests
+	{
+		private FileWriter _writer;
+		private string _file;
+		private string _content;
+		
+		[SetUp]
+		public void Setup()
+		{
+			_writer = new FileWriter();
+			_file = Path.GetTempFileName();
+			_content = Path.GetTempFileName();
+			File.WriteAllText(_file, getOriginal());
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			File.Delete(_file);
+			File.Delete(_content);
+		}
+		
+		[Test]
+		public void When_asked_to_insert_a_piece_of_text_it_will_insert_it()
+		{
+			File.WriteAllText(_content, "(inserted text here)");
+			var message = new EditorInsertMessage(
+				"(inserted text here)",
+				new GoTo()
+					{
+						File = _file,
+						Line = 2,
+						Column = 17
+					});
+			_writer.Insert(message);
+			Assert.That(
+				File.ReadAllText(_file),
+				Is.EqualTo(
+					"some text is already" + Environment.NewLine +
+					"in here but will (inserted text here)be modified" + Environment.NewLine +
+					"by these lovelly commands"));
+		}
+		
+		[Test]
+		public void When_asked_to_insert_a_piece_of_text_after_the_end_of_the_file_it_wont_be_inserted()
+		{
+			File.WriteAllText(_content, "(inserted text here)");
+			var message = new EditorInsertMessage(
+				"(inserted text here)",
+				new GoTo()
+					{
+						File = _file,
+						Line = 4,
+						Column = 17
+					});
+			_writer.Insert(message);
+			Assert.That(
+				File.ReadAllText(_file),
+				Is.EqualTo(
+					"some text is already" + Environment.NewLine +
+					"in here but will be modified" + Environment.NewLine +
+					"by these lovelly commands"));
+		}
+		
+		[Test]
+		public void When_asked_to_delete_code_in_a_line_it_will_delete_it()
+		{
+			var message = new EditorRemoveMessage(
+				new GoTo()
+					{
+						File = _file,
+						Line = 2,
+						Column = 17
+					},
+				new GoTo()
+					{
+						Line = 2,
+						Column = 21
+					});
+			_writer.Remove(message);
+			Assert.That(
+				File.ReadAllText(_file),
+				Is.EqualTo(
+					"some text is already" + Environment.NewLine +
+					"in here but will odified" + Environment.NewLine +
+					"by these lovelly commands"));
+		}
+		
+		[Test]
+		public void When_asked_to_delete_code_in_multiple_line_it_will_delete_it()
+		{
+			var message = new EditorRemoveMessage(
+				new GoTo()
+					{
+						File = _file,
+						Line = 1,
+						Column = 10
+					},
+				new GoTo()
+					{
+						Line = 2,
+						Column = 21
+					});
+			_writer.Remove(message);
+			Assert.That(
+				File.ReadAllText(_file),
+				Is.EqualTo(
+					"some text odified" + Environment.NewLine +
+					"by these lovelly commands"));
+		}
+
+		private string getOriginal()
+		{
+			return "some text is already" + Environment.NewLine +
+				   "in here but will be modified" + Environment.NewLine +
+				   "by these lovelly commands";
+		}
+	}
+}
--- src/EditorEngine.Core/Editors/IFileWriter.cs
+++ src/EditorEngine.Core/Editors/IFileWriter.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using EditorEngine.Core.Messaging.Messages;
 
 namespace EditorEngine.Core.Editors
@@ -7,5 +9,85 @@ namespace EditorEngine.Core.Editors
 	{
 		void Insert(EditorInsertMessage message);
 		void Remove(EditorRemoveMessage msg);
+	}
+
+	public class FileWriter : IFileWriter
+	{
+		public void Insert(EditorInsertMessage message)
+		{
+			var tempFile = Path.GetTempFileName();
+			using (var reader = new StreamReader(message.Destination.File))
+			{
+				using (var writer = new StreamWriter(tempFile))
+				{
+					long lineNr = 0;
+					string line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						lineNr++;
+						if (lineNr != 1)
+							writer.Write(Environment.NewLine);
+						if (lineNr == message.Destination.Line)
+						{
+							var prefix = "";
+							for (int i = 0; i < message.Destination.Column; i++)
+								prefix += line[i];
+							writer.Write(prefix);
+							writer.Write(message.Text);
+							writer.Write(line.Substring(prefix.Length, line.Length - prefix.Length));
+						}
+						else
+							writer.Write(line);
+					}
+				}
+				File.Delete(message.Destination.File);
+				File.Move(tempFile, message.Destination.File);
+			}
+		}
+
+		public void Remove(EditorRemoveMessage message)
+		{
+			var tempFile = Path.GetTempFileName();
+			using (var reader = new StreamReader(message.Start.File))
+			{
+				using (var writer = new StreamWriter(tempFile))
+				{
+					long lineNr = 0;
+					string line;
+					var singleLineDelete = message.Start.Line == message.End.Line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						lineNr++;
+						if (lineNr != 1 &&
+							(singleLineDelete ||
+							 lineNr < message.Start.Line ||
+							 lineNr > message.End.Line))
+							writer.Write(Environment.NewLine);
+						if (lineNr >= message.Start.Line &&
+							lineNr <= message.End.Line)
+						{
+							int column = 0;
+							foreach (char chr in line)
+							{
+								column++;
+								var delete = true;
+								if (lineNr == message.Start.Line && lineNr == message.End.Line)
+									delete = column > message.Start.Column && column <= message.End.Column;
+								else if (lineNr == message.Start.Line)
+									delete = column > message.Start.Column;
+								else if (lineNr == message.End.Line)
+									delete = column <= message.End.Column;
+								if (!delete)
+									writer.Write(chr);
+							}
+						}
+						else
+							writer.Write(line);
+					}
+				}
+				File.Delete(message.Start.File);
+				File.Move(tempFile, message.Start.File);
+			}
+		}
 	}
 }
