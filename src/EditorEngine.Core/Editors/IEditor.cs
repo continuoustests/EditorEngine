2c39aa7ecc809004bf9a5512e604183bdaa0171b
Svein Arne Ackenhausen <acken@acken.no>
2012-02-05 22:21:15 +0100

    Added more comments to the IEditor interface

--- src/EditorEngine.Core/Editors/IEditor.cs
+++ src/EditorEngine.Core/Editors/IEditor.cs
@@ -7,23 +7,40 @@ namespace EditorEngine.Core.Editors
 {
 	public interface IEditor
 	{
+		// Injection method for the tcp dispatcher to be injected
 		ICommandEndpoint Publisher { set; }
+		
+		// Is the editor still running?
 		bool IsAlive { get; }
 		
+		// Launch the editor and if not null go to location
 		void Initialize(Location location);
 		
+		// Set the editor window as active window
 		void SetFocus();
+		
+		// Have the editor go to / open a file at the specified location
 		void GoTo(Location location);
 
+		// A batch of commands is about to be sent to the editor
 		void BeginBatchUpdate();
+		
+		// Batch of commands completed
 		void EndBatchUpdate();
 
+		// Does this editor support text injection
 		bool CanInsertFor(string file);
+		
+		// Insert text into a file
 		void Insert(EditorInsertMessage msg);
 
+		// Does this editor support removing text
 		bool CanRemoveFor(string file);
+		
+		// Remove text from file
 		void Remove(EditorRemoveMessage msg);
 
+		// Return list of open unsaved files
 		KeyValuePair<string,string>[] GetDirtyFiles();
 	}
 }
